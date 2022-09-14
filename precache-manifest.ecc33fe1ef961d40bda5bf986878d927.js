f5846f2f581fff8de03973012d1df1715f0a78b8
Shireen Khan <shireenkhan.412@gmail.com>
2022-09-14 13:07:44 +0500

    Updates

---
+++ precache-manifest.ecc33fe1ef961d40bda5bf986878d927.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eab473c7831efa4ce3c",
+    "url": "/Week2-Lawbot-Website-React/static/css/main.2a6bee8b.chunk.css"
+  },
+  {
+    "revision": "9eab473c7831efa4ce3c",
+    "url": "/Week2-Lawbot-Website-React/static/js/main.9eab473c.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Week2-Lawbot-Website-React/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "cda18abbb68010086753",
+    "url": "/Week2-Lawbot-Website-React/static/js/2.cda18abb.chunk.js"
+  },
+  {
+    "revision": "a00f10f9ee0a9c596487",
+    "url": "/Week2-Lawbot-Website-React/static/js/runtime~main.a00f10f9.js"
+  },
+  {
+    "revision": "3c0535160fec52ae423e32bf753fd098",
+    "url": "/Week2-Lawbot-Website-React/static/media/lawbot.3c053516.jpg"
+  },
+  {
+    "revision": "9b1a6bc5a9dfbab19547f03092c269ab",
+    "url": "/Week2-Lawbot-Website-React/static/media/bg8.9b1a6bc5.jpg"
+  },
+  {
+    "revision": "0179e43f1c5c6d9fa892d6caf7be8ab5",
+    "url": "/Week2-Lawbot-Website-React/index.html"
+  }
+];
